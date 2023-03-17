a3d91ce72bf86e49af6d5f3a97183339970a6d78
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-16 19:19:50 -1200

    Welcome page

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using NonStaticEmpWageProblem;
+
+namespace EmployeeWageComutationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to employee wage computation problem");
+            EmpAttendence Attendance = new EmpAttendence();
+            Attendance.EmployeeCheck();
+        }
+    }
+}
